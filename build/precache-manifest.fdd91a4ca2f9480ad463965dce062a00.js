eacd14221c922603834a0f9618db6105f2411252
JulieBio <juliebio69@gmail.com>
2018-11-29 21:16:03 +0100

    first commit

---
+++ build/precache-manifest.fdd91a4ca2f9480ad463965dce062a00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8434f823b8d21615794b",
+    "url": "/juliebio_spellsduel.github.io/static/js/runtime~main.8434f823.js"
+  },
+  {
+    "revision": "ab18a33e7397180a7d37",
+    "url": "/juliebio_spellsduel.github.io/static/js/main.ab18a33e.chunk.js"
+  },
+  {
+    "revision": "7131e0636e790aa0db44",
+    "url": "/juliebio_spellsduel.github.io/static/js/1.7131e063.chunk.js"
+  },
+  {
+    "revision": "ab18a33e7397180a7d37",
+    "url": "/juliebio_spellsduel.github.io/static/css/main.89e993ef.chunk.css"
+  },
+  {
+    "revision": "7131e0636e790aa0db44",
+    "url": "/juliebio_spellsduel.github.io/static/css/1.2f34c7ca.chunk.css"
+  },
+  {
+    "revision": "ab8098e1da16658458b65e92c990e1ce",
+    "url": "/juliebio_spellsduel.github.io/index.html"
+  }
+];
