8241200a37d96a75257ebd4138429f461fe2335d
JulieBio <juliebio69@gmail.com>
2018-12-05 22:09:42 +0100

    Updates

---
+++ precache-manifest.cdef66899bc00015d8c8dff89f389423.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b029eb8d656cd4d6cf0b",
+    "url": "/spellsduel/static/js/runtime~main.b029eb8d.js"
+  },
+  {
+    "revision": "546eefd554d816a7eb03",
+    "url": "/spellsduel/static/js/main.546eefd5.chunk.js"
+  },
+  {
+    "revision": "5300af4e30860c23b9d2",
+    "url": "/spellsduel/static/js/1.5300af4e.chunk.js"
+  },
+  {
+    "revision": "546eefd554d816a7eb03",
+    "url": "/spellsduel/static/css/main.7b93b427.chunk.css"
+  },
+  {
+    "revision": "5300af4e30860c23b9d2",
+    "url": "/spellsduel/static/css/1.69dc9f5b.chunk.css"
+  },
+  {
+    "revision": "53fc2a29e6ff37387a0f4b7ae88093b2",
+    "url": "/spellsduel/index.html"
+  }
+];
